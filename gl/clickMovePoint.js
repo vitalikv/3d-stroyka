a50253ed6e457c8bd9709de58b88bf76d7083f60
Vitalii <9455469@mail.ru>
2019-02-22 14:21:09 +0300

    7.1 clickPointMouseUp

--- gl/clickMovePoint.js
+++ gl/clickMovePoint.js
@@ -703,7 +703,7 @@ function undoRedoChangeMovePoint( point, walls )
 
 function clickPointMouseUp(obj)
 {  
-	//if(obj.w.length > 0) createWallZone(obj.w[0]);
+	if(obj.w.length > 0) createWallZone(obj.w[0]);
 }
 
 
