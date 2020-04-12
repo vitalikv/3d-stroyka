424270e31faac9c02f4dd22680543d16c6c98b7c
vit <9455469@mail.ru>
2020-04-12 23:23:33 +0300

    53.3

--- gl/uiInterface_list_obj.js
+++ gl/uiInterface_list_obj.js
@@ -12,7 +12,7 @@ function updateListTubeUI_1(cdm)
 		var tag = obj.userData.tag; 
 		
 		var str_button = 
-		'<div nameId="shCp_1" style="width: 40px; height: 20px;">\
+		'<div nameId="sh_select_obj3D" style="width: 40px; height: 20px;">\
 			<div style="position: absolute; width: 15px; height: 10px; right: 20px;">\
 				<svg height="100%" width="100%" viewBox="0 0 100 100">\
 					<polygon points="0,0 100,0 50,100" style="fill:#ffffff;stroke:#000000;stroke-width:4" />\
@@ -26,7 +26,7 @@ function updateListTubeUI_1(cdm)
 			var str = 
 			'<div>\
 				<div class="right_panel_1_1_list_item">\
-					<div class="flex_1 relative_1" uuid="'+obj.uuid+'">\
+					<div class="flex_1 relative_1">\
 						<div class="right_panel_1_1_list_item_color" item="color"></div>\
 						<div class="right_panel_1_1_list_item_text" item="name">труба</div>\
 						<div class="right_panel_1_1_list_item_text" item="value"></div>\
@@ -40,7 +40,7 @@ function updateListTubeUI_1(cdm)
 			var str = 
 			'<div>\
 				<div class="right_panel_1_1_list_item">\
-					<div class="flex_1 relative_1" uuid="'+obj.uuid+'">\
+					<div class="flex_1 relative_1">\
 						<div class="right_panel_1_1_list_item_text">'+obj.userData.obj3D.nameRus+'</div>\
 						'+str_button+'\
 					</div>\
@@ -50,14 +50,30 @@ function updateListTubeUI_1(cdm)
 		else
 		{
 			return;
-		}
+		}		
+		
+		var q = $(str).prependTo('[list_ui="wf"]')[0];		
+		
+		if(tag == 'wf_line') { $(q.querySelector('[item="color"]')).css('background-color', '#'+obj.userData.wf_line.color.clone().getHexString()); }
+
+
+		// назначаем событие при клике на название объекта UI
+		(function(obj) 
+		{
+			$(q).on('mousedown', function(e)
+			{   
+				clickObject3D( obj, { menu_1: true, outline: true} );
+				e.stopPropagation();
+			});	
+		}(obj));
 		
 		
-		var q = $(str).prependTo('[list_ui="wf"]')[0];
-		q.uuid = obj.uuid;
-		
-		
-		if(tag == 'wf_line') { $(q.querySelector('[item="color"]')).css('background-color', '#'+obj.userData.wf_line.color.clone().getHexString()); }	
+		// назначаем кнопки треугольник событие
+		var el_2 = $(q.querySelector('[nameId="sh_select_obj3D"]'));
+		(function(obj) 
+		{
+			el_2.on('mousedown', function(e){ fitCameraToObject({obj: obj}); e.stopPropagation(); });	
+		}(obj));		
 		
 		var n = infProject.list.obj_scene_ui.length;
 		infProject.list.obj_scene_ui[n] = { el: q, o: obj };
